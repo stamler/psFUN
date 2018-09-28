53c7078d0ec625e9e2fd1c91229c414f991ab7a7
Dean Stamler <dean@stamler.ca>
2018-09-28 11:00:53 -0400

    include deletion code, make output CSV-like

--- cleanup.ps1
+++ cleanup.ps1
@@ -5,11 +5,12 @@ $t_dsstore_size = $t_dsstore_count = 0
 $t_thumbs_size = $t_thumbs_count = 0
 $t_bak_size = $t_bak_count = 0
 $t_lnk_size = $t_lnk_count = 0
+# $csv_name = 'clean-{0}.csv' -f (Get-Date -format "yyyy-MM-dd_HH:mm")
 
 Get-ChildItem -Path . -Include Thumbs.db,.DS_Store,~*,*.tmp,*.bak,*.lnk -Recurse -Force | ForEach-Object { 
   $t_count +=1; 
   $t_size += $_.length; 
-  '{0,10} {1,4}' -f $_.Length, $_.Name
+  '{0},{1}' -f $_.Length, $_.FullName
 
   if ($_.Name -eq 'Thumbs.db') { $t_thumbs_count += 1; $t_thumbs_size += $_.Length }
   elseif ($_.Name -eq '.DS_Store') { $t_dsstore_count += 1; $t_dsstore_size += $_.Length }
@@ -17,6 +18,8 @@ Get-ChildItem -Path . -Include Thumbs.db,.DS_Store,~*,*.tmp,*.bak,*.lnk -Recurse
   elseif ($_.Name -match '\.lnk$') { $t_lnk_count += 1; $t_lnk_size += $_.Length }
   elseif ($_.Name -match '\.bak$') { $t_bak_count += 1; $t_bak_size += $_.Length }
   elseif ($_.Name -match '^~') { $t_tilde_count += 1; $t_tilde_size += $_.Length }
+
+  Remove-Item $_ -Force
 }
 
 'Found {0} matching files with total size of {1} bytes' -f $t_count, $t_size
