2620afe1d01259fb20999ece51546cf80d209700
Dean Stamler <dean@stamler.ca>
2020-04-15 12:21:00 -0400

    network info, message output, rename computer

--- MachineBaseSetup.ps1
+++ MachineBaseSetup.ps1
@@ -2,10 +2,13 @@
 # then display the new password. If it doesn't exist, create it and show the
 # password
 
+# Create Output Object
+$output = @{}
+
 # Generate a password
 Add-Type -AssemblyName 'System.Web'
-$Password = [System.Web.Security.Membership]::GeneratePassword(14, 1)
-$SecurePassword = $Password | ConvertTo-SecureString -AsPlainText -Force
+$output["Password"] = [System.Web.Security.Membership]::GeneratePassword(14, 1)
+$SecurePassword = $output["Password"] | ConvertTo-SecureString -AsPlainText -Force
 
 try {
     # Powershell Try/Catch only works with terminating errors, use ErrorAction
@@ -27,21 +30,41 @@ catch {
 
 # TODO: Enable WinRM per ansible docs then alert user
 
-# Get computer info and display output
+# Get computer info
 $c_bios = Get-WmiObject Win32_Bios
 $c_system = Get-WmiObject Win32_ComputerSystem
+$c_netset = Get-WmiObject Win32_NetworkAdapterConfiguration -Filter "IPEnabled = True"
 
-# Generate new computer name from MFG/Serial/PCSystemType
-$NewComputerName = ( 
+# Get IP Addresses
+ForEach ($c_net in $c_netset) {
+    # Iterate over array elements and assign type stringValue
+    ForEach ($t in $c_net.IPAddress) {
+        [array]$ip_list += $t
+    }
+    $output[$c_net.MACAddress] = $ip_list
+}
+
+# Generate proposed computer name from MFG/Serial/PCSystemType
+$output["ProposedComputerName"] = ( 
         $( If ($c_system.PCSystemType -eq 2) {"M"} Else {"F"} ) + 
         $c_system.Manufacturer.Substring(0,2).ToUpper() + 
         "-" +  $c_bios.SerialNumber
     )
 
-Write-Output $IPAddresses
-Write-Output "ansible account password: $($Password)"
-Write-Output "proposed computer name: $($NewComputerName)"
+Write-Host "`n*********************************************************************" -ForegroundColor Yellow
+Write-Host "Please copy the following information to IT, it will only appear once" -ForegroundColor Magenta
+Write-Host "*********************************************************************" -ForegroundColor Yellow
+Write-Output $output
 
-# TODO: Prompt to rename computer (Type 'Yes')
- 
+# Prompt to rename computer if proposed name is different
+if ($env:COMPUTERNAME -ne $output["ProposedComputerName"]) {
+    Write-Host "`nExisting name $($env:COMPUTERNAME) doesn't match $($output['ProposedComputerName'])"
+    $confirm = Read-Host "`nRename the computer and reboot? [Yes/no]"
+    if ($confirm -eq 'Yes') {
+        Write-Host "Renaming the computer..."
+        Rename-Computer -NewName $output["ProposedComputerName"] -Force -Restart
+    } else {
+        Write-Host "The computer was not renamed."
+    }
+}
  
