af75078a440fe1a9a709aeb5c05ede7e431693bf
Dean Stamler <dev@stamler.ca>
2022-03-15 15:08:09 -0400

    comments

--- Enable-GPOBitlocker.ps1
+++ Enable-GPOBitlocker.ps1
@@ -1,5 +1,9 @@
+# Enable bitlocker on computers that have been configured for it by running this
+# script from scheduled tasks
 $BLV = Get-BitLockerVolume -MountPoint 'C:'
 if ($BLV.volumeStatus -eq 'FullyDecrypted') {
+    # Create the key protector to be pushed to Active Directory
     Add-BitLockerKeyProtector -MountPoint 'c:' -RecoveryPasswordProtector
+    # Enable Bitlocker
     Enable-Bitlocker -MountPoint 'c:' -TpmProtector
 }
