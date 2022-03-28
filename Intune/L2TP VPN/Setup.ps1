d86f3910459314d1e55243802b222a9aaecc4946
Dean Stamler <dev@stamler.ca>
2022-03-28 17:17:19 -0400

    Setup VPN

--- Intune/L2TP VPN/Setup.ps1
+++ Intune/L2TP VPN/Setup.ps1
@@ -6,10 +6,45 @@ param (
 )
 
 $ConnectionName = "TBT Engineering Limited L2TP"
+$server = "vpn.tbte.ca"
+
+function Write-LogEntry {
+  param (
+      [parameter(Mandatory = $true)]
+      [ValidateNotNullOrEmpty()]
+      [string]$Value,
+      [parameter(Mandatory = $false)]
+      [ValidateNotNullOrEmpty()]
+      [string]$FileName = "$($ConnectionName).log",
+      [switch]$Stamp
+  )
+
+  #Build Log File appending System Date/Time to output
+  $LogFile = Join-Path -Path $env:SystemRoot -ChildPath $("Temp\$FileName")
+  $Time = -join @((Get-Date -Format "HH:mm:ss.fff"), " ", (Get-WmiObject -Class Win32_TimeZone | Select-Object -ExpandProperty Bias))
+  $Date = (Get-Date -Format "MM-dd-yyyy")
+
+  If ($Stamp) {
+      $LogText = "<$($Value)> <time=""$($Time)"" date=""$($Date)"">"
+  }
+  else {
+      $LogText = "$($Value)"   
+  }
+
+  Try {
+      Out-File -InputObject $LogText -Append -NoClobber -Encoding Default -FilePath $LogFile -ErrorAction Stop
+  }
+  Catch [System.Exception] {
+      Write-Warning -Message "Unable to add log entry to $LogFile.log file. Error message at line $($_.InvocationInfo.ScriptLineNumber): $($_.Exception.Message)"
+  }
+}
+
 
 try {
-  Add-VpnConnection -Name "TBT Engineering Limited L2TP" -ServerAddress "vpn.tbte.ca" -AllUserConnection -TunnelType "L2tp" -L2tpPsk $l2tppsk -EncryptionLevel "Required" -AuthenticationMethod "Chap","MSChapv2" -Force
+  Add-VpnConnection -Name $ConnectionName -ServerAddress $server -AllUserConnection -TunnelType "L2tp" -L2tpPsk $l2tppsk -EncryptionLevel "Required" -AuthenticationMethod "Chap","MSChapv2" -Force
+  Write-LogEntry -Stamp -Value "Created L2TP connection $($ConnectionName)"
 }
 catch [Microsoft.Management.Infrastructure.CimException] {
-  Write-Host "VPN connection $ConnectionName already exists"
+  Write-LogEntry -Stamp -Value "Connection already exists, rewriting values"
+  Set-VpnConnection -Name $ConnectionName -ServerAddress $server -AllUserConnection -TunnelType "L2tp" -L2tpPsk $l2tppsk -EncryptionLevel "Required" -AuthenticationMethod "Chap","MSChapv2" -Force
 }
