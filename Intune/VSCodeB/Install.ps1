16554b21b4d07812341f556d4f4d69b8f4b6d270
Dean Stamler <dev@stamler.ca>
2022-03-28 21:58:37 -0400

    Add regular silent VSCode install

---
+++ Intune/VSCodeB/Install.ps1
@@ -0,0 +1,1 @@
+ .\VSCodeSetup-x64-1.65.2.exe /verysilent /norestart /mergetasks=!runcode /log="$($env:TEMP)VSCodeInstall.log" 
---
+++ Intune/VSCodeB/Uninstall.ps1
@@ -0,0 +1,1 @@
+ & 'C:\Program Files\Microsoft VS Code\unins000.exe' /verysilent /norestart
