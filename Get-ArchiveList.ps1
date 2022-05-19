6c9e1c5770974908ee4e82bdcafa34d27d3a8563
Dean Stamler <dev@stamler.ca>
2022-05-19 09:19:45 -0400

    output reporting

--- Get-ArchiveList.ps1
+++ Get-ArchiveList.ps1
@@ -2,29 +2,33 @@
 # the most recently modified file in the job then output it to a CSV file. The
 # edit time of the parent job folder is not considered.
 
-$ageindays = 1000
+$ageindays = 600
 $moveitems = $false
-$destination = "\\nas2.main.tbte.ca\Archive\Proposals\2019\r\"
-$parentdir = “F:\projects\Proposals\2019”
-$pattern = "P\d\d-\d{3,4}(-\d{1,3})?"
+$destination = "\\nas2.main.tbte.ca\Archive\Projects\2019\r\"
+$parentdir = “F:\projects\Projects\2019”
+#$pattern = "P\d\d-\d{3,4}(-\d{1,3})?"
+$pattern = "\d\d-\d{3,4}(-\d{1,3})?"
 $output = @()
 foreach($project in Get-ChildItem -Directory $parentdir | where-object {$_.name -match $pattern}) {
   $bytesize = 0
-  $projsize = Get-ChildItem -Path $project.FullName -Recurse | Measure-Object -Sum Length | Select-Object Sum, Count
+  $reportString = "---`r`n$($project.FullName)`r`n"
+  $projsize = Get-ChildItem -Path $project.FullName -Recurse | Where-Object { $_.PsIsContainer -eq $false }| Measure-Object -Sum Length | Select-Object Sum, Count
   if ($projsize.Count -gt 0) {
-    # There are items in the project folder
+    $reportString += "There are $($projsize.Count) items totalling $($projsize.Sum) bytes in the project folder"
     $bytesize = $projsize.Sum
     $newestitem = Get-ChildItem -Path $project.FullName -Recurse | sort LastWriteTime | select -last 1
     if ($project.LastWriteTime -gt $newestitem.LastWriteTime) {
-      # The project folder itself is newer than the newest file in the project
+      $reportString += "`r`nThe project folder itself is newer than the newest file in the project"
       $newestitem = $project
     }
   } else {
+    $reportString += "This folder is empty so the date of the folder is being used"
     # There are no items in the project folder
     $newestitem = $project
   }
   if ($newestitem.LastWriteTime -lt (get-date).AddDays(-$ageindays)) {
-    # The project is older than the ageindays
+    $timeSpan = New-TimeSpan -End $(get-date) -Start $newestitem.LastWriteTime
+    $reportString += "`r`nNo edits have been performed in $($timeSpan.Days) days`r`n"
     $output += [PSCustomObject]@{
       job = $project.FullName
       time = $newestitem.LastWriteTime
@@ -34,7 +38,13 @@ foreach($project in Get-ChildItem -Directory $parentdir | where-object {$_.name 
     if ($moveitems -eq $true) {
       # Move the job to the staging area in the archive
       Move-Item -Path $project.FullName -Destination $destination
-    }  
+      $reportString += "Item moved"
+    } else {
+      $reportString += "Dry Run: $($project.FullName) > $destination"
+    }
+  } else {
+    $reportString += "`r`nThe folder was edited within the last $ageindays days"
   }
+  Write-Host $reportString
 }
-$output | Export-Csv output.csv
+$output | Export-Csv output.csv 
