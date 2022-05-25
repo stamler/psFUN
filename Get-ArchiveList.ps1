b9bc083469467c7983634fbd34e644517316604a
Dean Stamler <dev@stamler.ca>
2022-05-25 16:14:54 -0400

    only create r dir if moving, less verbose

--- Get-ArchiveList.ps1
+++ Get-ArchiveList.ps1
@@ -11,14 +11,13 @@ function Archive-Jobs {
     [switch]$Move = $false
   )
 
-  $ageindays = $Age
   $year = $Year
   $jobType = if ($Proposals) { "Proposals" } else { "Projects" }
   $moveitems = $Move
 
   $destination = "\\nas2.main.tbte.ca\Archive\$jobType\$year\r\"
   # Create the folder if it doesn't exist
-  if (!(Test-Path $destination)) {
+  if (!(Test-Path $destination) -and $moveitems) {
     New-Item -ItemType Directory -Path $destination
   }
 
@@ -32,61 +31,62 @@ function Archive-Jobs {
   foreach($project in $projects) {
     $itemNumber ++
     $percentComplete = [int]($itemNumber * 100 / $folderCount)
-    Write-Progress -Id 1 -Activity Archiving -Status $project.FullName -PercentComplete $percentComplete -CurrentOperation "Analyzing"
+    Write-Progress -Id 1 -Activity "Archiving $year $jobType" -Status $project.FullName -PercentComplete $percentComplete -CurrentOperation "Analyzing"
     $bytesize = 0
-    $reportString = "---`r`n$($project.FullName)`r`n"
     $projsize = Get-ChildItem -Path $project.FullName -Recurse | Where-Object { $_.PsIsContainer -eq $false }| Measure-Object -Sum Length | Select-Object Sum, Count
     if ($projsize.Count -gt 0) {
-      $reportString += "There are $($projsize.Count) items totalling $($projsize.Sum) bytes in the project folder"
+      $emptyFolder = $false
       $bytesize = $projsize.Sum
       $newestitem = Get-ChildItem -Path $project.FullName -Recurse | sort LastWriteTime | select -last 1
+      $projectFolderIsNewest = $false
       if ($project.LastWriteTime -gt $newestitem.LastWriteTime) {
-        $reportString += "`r`nThe project folder itself is newer than the newest file in the project"
+        $projectFolderIsNewest = $true
         $newestitem = $project
       }
     } else {
-      $reportString += "This folder is empty so the date of the folder is being used"
+      $emptyFolder = $true
+      $projectFolderIsNewest = $true
       # There are no items in the project folder
       $newestitem = $project
     }
-    if ($newestitem.LastWriteTime -lt (get-date).AddDays(-$ageindays)) {
+    if ($newestitem.LastWriteTime -lt (get-date).AddDays(-$Age)) {
       $timeSpan = New-TimeSpan -End $(get-date) -Start $newestitem.LastWriteTime
-      $reportString += "`r`nNo edits have been performed in $($timeSpan.Days) days`r`n"
+      $daysOld = $timeSpan.Days
+      $moveErrorsEncountered = 0
+      if ($moveitems -eq $true) {
+        # Move the job to the staging area in the archive
+        Write-Progress -Id 1 -Activity "Archiving $year $jobType" -Status $project.FullName -PercentComplete $percentComplete -CurrentOperation "Moving"
+        $moveErrorsCount = $moveErrors.Count
+        Move-Item -Path $project.FullName -Destination $destination -ErrorAction SilentlyContinue -ErrorVariable +moveErrors
+        if ($moveErrors.Count -gt $moveErrorsCount) {
+          $moveErrorsEncountered = $moveErrors.Count - $moveErrorsCount
+          $jobsWithErrors += $project.FullName
+        }
+      }
       $output += [PSCustomObject]@{
         job = $project.FullName
         time = $newestitem.LastWriteTime
         size = $bytesize
         count = $projsize.Count
+        projectFolderIsNewest = $projectFolderIsNewest
+        emptyFolder = $emptyFolder
+        daysOld = $daysOld
+        moveErrorsEncountered = $moveErrorsEncountered
       }
-      if ($moveitems -eq $true) {
-        # Move the job to the staging area in the archive
-        Write-Progress -Id 1 -Activity Archiving -Status $project.FullName -PercentComplete $percentComplete -CurrentOperation "Moving"
-        $moveErrorsCount = $moveErrors.Count
-        Move-Item -Path $project.FullName -Destination $destination -ErrorAction SilentlyContinue -ErrorVariable +moveErrors
-        if ($moveErrors.Count -gt $moveErrorsCount) {
-          $reportString += "`r`nThere were error(s) moving the folder to the archive"
-          $jobsWithErrors += $project.FullName
-        }
-        $reportString += "Item moved"
-      } else {
-        $reportString += "Dry Run: $($project.FullName) > $destination"
-      }
-    } else {
-      $reportString += "`r`nThe folder was edited within the last $ageindays days"
     }
-    Write-Host $reportString
   }
   if ($moveErrors.Count -gt 0) {
-    Write-Output "`r`nMove-Item Errors Unique TargetObjects:"
-    Write-Output $jobsWithErrors | Sort-Object -Unique
-    # TODO: TargetObject only contains the file name, not the full path.
+    # TargetObject only contains the file name, not the full path.
     # We must have the full path to the file in order to remediate the error.
+    Write-Output "Move-Item unique jobs with errors:"
+    Write-Output $jobsWithErrors | Sort-Object -Unique
+    Write-Output "Move-Item unique TargetObjects with errors:"
     Write-Output $moveErrors | Select-Object -Property TargetObject -Unique
-  } else {
-    Write-Output "No move errors"
   }
   $csvPath = $(Get-Location).Path + "\$year-$jobType-" + $(Get-Date -Format "yyyy-MM-dd HH-mm") + $(if ($moveitems -eq $true) { " Archive" } else { " DryRun" }) + ".csv"
   $output | Export-Csv -Path $csvPath
 }
 
 Archive-Jobs -Proposals -Year 2019
+Archive-Jobs -Proposals -Year 2020
+Archive-Jobs -Proposals -Year 2021
