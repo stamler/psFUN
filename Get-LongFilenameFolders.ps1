d8381f9b3c2e8d643ce3c3d4599a60dad73c964d
Dean Stamler <dev@stamler.ca>
2022-05-30 17:12:36 -0400

    Increase flexibility of Get-LongFilenameFolders

--- Get-LongFilenameFolders.ps1
+++ Get-LongFilenameFolders.ps1
@@ -1,2 +1,3 @@
-$path = "F:\projects\Projects\2017"
-Get-ChildItem -Path $path -Recurse | Where-Object {$_.FullName.Length -gt 260 } | Select-Object @{Name=”MaxLength”;Expression={$_.fullname.length}}, @{Name="Folder";Expression={$_.FullName | Split-Path -Parent}} | Group-Object -Property Folder | ForEach-Object {$_.Group | Sort-Object MaxLength -Descending | Select-Object -First 1}
+"2015","2016","2017","2018","2019","2020","2021","2022" | ForEach-Object {
+  Get-ChildItem -Path "F:\projects\Projects\$_" -Recurse | Where-Object {$_.FullName.Length -gt 260 } | Select-Object @{Name=”MaxLength”;Expression={$_.fullname.length}}, @{Name="Folder";Expression={$_.FullName | Split-Path -Parent}} | Group-Object -Property Folder | ForEach-Object {$_.Group | Sort-Object MaxLength -Descending | Select-Object -First 1} 
+} 
